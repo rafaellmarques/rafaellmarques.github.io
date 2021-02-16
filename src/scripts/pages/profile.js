78e131f9d91652c3082c30753f4172db73147ed2
Rafael Lourenço Marques <rafaellmarques@outlook.com>
2021-02-16 20:33:04 -0300

    ajustes no card;

--- src/scripts/pages/profile.js
+++ src/scripts/pages/profile.js
@@ -1,6 +1,8 @@
 'use strict';
 
 import utilities from '../modules/_utilities';
+
+import simplebar from 'simplebar'
 import tilt from 'vanilla-tilt';
 
 const profile = {
@@ -20,7 +22,7 @@ const profile = {
 		})
 			.then((response) => response.json())
 			.then((data) => {
-				// console.log(data);
+				// console.info(data);
 				const icoBio = `
 				<?xml version='1.0' encoding='UTF-8'?>
 				<svg viewBox="-2 -5.6375 32 32" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
